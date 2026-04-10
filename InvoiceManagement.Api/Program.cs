c1fc09f0df5ebe15c85df85d1f9cc3ac22762f92
Daniel <daniel-zanelato@hotmail.com>
2026-04-10 14:24:19 -0300

    Ajustando CORS policy pra qualquer dominio

--- InvoiceManagement.Api/Program.cs
+++ InvoiceManagement.Api/Program.cs
@@ -29,7 +29,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("FrontendPolicy", policy =>
     {
         policy
-            .WithOrigins("http://localhost:3000")
+            .AllowAnyOrigin()
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
